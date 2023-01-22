fd0e702aa19821126499da2477e639080a0234b1
Pi Lanningham <pi@sundaeswap.finance>
2023-01-22 04:50:41 -0500

    Save block headers to disk

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,25 @@
-use pallas::network::{
-    miniprotocols::{blockfetch, chainsync, handshake, txmonitor, Point, MAINNET_MAGIC},
-    multiplexer::{bearers::Bearer, StdChannel, StdPlexer},
+use std::{
+    fmt, fs,
+    path::{self, Path, PathBuf},
 };
 
-#[derive(Debug)]
-struct LoggingObserver;
+use clap::Parser;
+use pallas::{
+    codec::minicbor::{self, decode, encode::Error},
+    crypto::hash::{Hash, Hasher},
+    ledger::{
+        primitives::byron::{BlockHead, EbbCons, EbbHead},
+        traverse::ComputeHash,
+    },
+    network::{
+        miniprotocols::{
+            blockfetch,
+            chainsync::{self, HeaderContent},
+            handshake, txmonitor, Point, MAINNET_MAGIC,
+        },
+        multiplexer::{bearers::Bearer, StdChannel, StdPlexer},
+    },
+};
 
 fn do_handshake(channel: StdChannel) {
     let mut client = handshake::N2NClient::new(channel);
@@ -46,36 +61,74 @@ fn do_blockfetch(channel: StdChannel) {
     }
 }
 
-fn do_chainsync(channel: StdChannel) {
-    let known_points = vec![Point::Origin];
+struct Slurp {
+    directory: PathBuf,
+}
 
-    let mut client = chainsync::N2NClient::new(channel);
+impl Slurp {
+    fn handle_header(&self, height: u32, h: HeaderContent) {
+        let ebb_hash =
+            minicbor::decode::<EbbHead>(&h.cbor).and_then(|b| Ok(b.compute_hash()));
+        let byron_block_hash = ebb_hash.or_else(|_| {
+            minicbor::decode::<BlockHead>(&h.cbor).and_then(|b| Ok(b.compute_hash()))
+        });
+        let modern_hash = byron_block_hash
+            .or_else(|_| Ok::<_, decode::Error>(Hasher::<256>::hash(&h.cbor)));
 
-    let (point, _) = client.find_intersect(known_points).unwrap();
+        let hash = modern_hash.expect("couldn't compute hash");
 
-    log::info!("intersected point is {:?}", point);
+        log::info!("rolling forward, {}", hash);
 
-    for _ in 0.. {
-        let next = client.request_next().unwrap();
+        let file =
+            self.directory
+                .join("headers")
+                .join(format!("{}-{}", height, hex::encode(hash)));
+        fs::write(file, h.cbor).expect("could not save header");
+    }
 
-        match next {
-            chainsync::NextResponse::RollForward(h, _) => {
-                log::info!("rolling forward, header size: {}", h.cbor.len())
-            }
-            chainsync::NextResponse::RollBackward(x, _) => log::info!("rollback to {:?}", x),
-            chainsync::NextResponse::Await => log::info!("tip of chaing reached"),
-        };
+    fn do_chainsync(&self, channel: StdChannel) {
+        let known_points = vec![Point::Origin];
+
+        let mut client = chainsync::N2NClient::new(channel);
+
+        let (point, _) = client.find_intersect(known_points).unwrap();
+
+        log::info!("intersected point is {:?}", point);
+
+        for height in 0.. {
+            let next = client.request_next().unwrap();
+
+            match next {
+                chainsync::NextResponse::RollForward(h, _) => self.handle_header(height, h),
+                chainsync::NextResponse::RollBackward(x, _) => log::info!("rollback to {:?}", x),
+                chainsync::NextResponse::Await => log::info!("tip of chaing reached"),
+            };
+        }
     }
 }
 
+#[derive(Parser)]
+#[command(author, version, about)]
+struct Args {
+    // The cardano relay node to connect to
+    #[arg(default_value = "relays-new.cardano-mainnet.iohk.io:3001")]
+    relay: String,
+
+    // The directory to save blocks into
+    #[arg(default_value = "blocks")]
+    directory: PathBuf,
+}
+
 fn main() {
+    let args = Args::parse();
+
     env_logger::builder()
         .filter_level(log::LevelFilter::Info)
         .init();
 
     // setup a TCP socket to act as data bearer between our agents and the remote
     // relay.
-    let bearer = Bearer::connect_tcp("relays-new.cardano-mainnet.iohk.io:3001").unwrap();
+    let bearer = Bearer::connect_tcp(args.relay).unwrap();
 
     // setup the multiplexer by specifying the bearer and the IDs of the
     // miniprotocols to use
@@ -87,12 +140,17 @@ fn main() {
     plexer.muxer.spawn();
     plexer.demuxer.spawn();
 
+    fs::create_dir_all(&args.directory).expect("could not create target directory");
+    fs::create_dir_all(&args.directory.join("headers")).expect("could not create target directory");
+
     // execute the required handshake against the relay
     do_handshake(channel0);
 
     // fetch an arbitrary batch of block
     do_blockfetch(channel3);
 
+    let slurp = Slurp { directory: args.directory };
+
     // execute the chainsync flow from an arbitrary point in the chain
-    do_chainsync(channel2);
+    slurp.do_chainsync(channel2);
 }
