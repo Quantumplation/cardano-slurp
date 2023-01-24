888d22240a8d2da0cdfefec0bcc3493a23d0a3ac
Pi Lanningham <pi.lanningham@gmail.com>
2023-01-24 05:39:43 -0500

    Fix a typo in a comment

--- src/utils.rs
+++ src/utils.rs
@@ -13,7 +13,7 @@ pub fn artifact_path(base_directory: PathBuf, point: Point) -> PathBuf {
     // typically you should try to avoid having more than 10k files in a directory
     // each upper bucket rolls over every 20_000_000 slots, which is around 230 days
     // each lower bucket rolls over every 200_000 slots, which is around 2 days
-    // each upper bucket will have 1000 subdirectories, and each subdirectory will have on average 10k files
+    // each upper bucket will have 100 subdirectories, and each subdirectory will have on average 10k files
     let upper_bucket = format!("{}", slot - (slot % LARGE_BUCKET_SIZE));
     let lower_bucket = format!("{}", slot - (slot % SMALL_BUCKET_SIZE));
     let sub_directory = base_directory.join(upper_bucket).join(lower_bucket);
